4438138956a754d5ebf994296f746bd470d58369
Курчин Иоанн Вячеславович <lvrainman@gmail.com>
2025-09-27 23:39:46 +0300

    Enter IO monad

---
+++ src/main/scala/com/rockthejvm/part2effects/IOIntroduction.scala
@@ -0,0 +1,50 @@
+package com.rockthejvm.part2effects
+
+import cats.effect.IO
+
+import scala.io.StdIn
+
+object IOIntroduction {
+  // IO
+  val ourFirstIO: IO[Int] = IO.pure(42) // arg that should not have side effects
+  val aDelayedIO: IO[Int] = IO.delay({
+    println("I'm producing an integer")
+    54
+  })
+
+  val shouldNotDoThis: IO[Int] = IO.pure({
+    println("I'm producing an integer")
+    54
+  })
+
+  val aDelayedIO_v2: IO[Int] = IO { // apply == delay
+    println("I'm producing an integer")
+    42
+  }
+
+  // map, flatMap
+  val improvedMeaningOfLife = ourFirstIO.map(_ * 2)
+  val printedMeaningOfLife = ourFirstIO.flatMap(mol => IO.delay(println(mol)))
+
+  def smallProgram(): IO[Unit] = for {
+    line1 <- IO(StdIn.readLine())
+    line2 <- IO(StdIn.readLine())
+    _ <- IO.delay(println(line1 + line2))
+  } yield ()
+
+  // mapN - combine IO Effects as tuples
+
+  import cats.syntax.apply.*
+
+  val combinedMeaningOfLIfe = (ourFirstIO, improvedMeaningOfLife).mapN(_ + _)
+
+  def smallProgram_v2(): IO[Unit] =
+    (IO(StdIn.readLine()), IO(StdIn.readLine())).mapN(_ + _).map(println)
+
+  def main(args: Array[String]): Unit = {
+    import cats.effect.unsafe.implicits.global
+    // "end of the world"
+    println(aDelayedIO.unsafeRunSync())
+  }
+
+}

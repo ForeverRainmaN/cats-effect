fa48f28e7a105ca70d1bcfae2deb9324a735c248
Курчин Иоанн Вячеславович <lvrainman@gmail.com>
2025-09-28 19:35:09 +0300

    IO exercises

--- src/main/scala/com/rockthejvm/part2effects/IOIntroduction.scala
+++ src/main/scala/com/rockthejvm/part2effects/IOIntroduction.scala
@@ -2,6 +2,7 @@ package com.rockthejvm.part2effects
 
 import cats.effect.IO
 
+import scala.annotation.tailrec
 import scala.io.StdIn
 
 object IOIntroduction {
@@ -41,10 +42,98 @@ object IOIntroduction {
   def smallProgram_v2(): IO[Unit] =
     (IO(StdIn.readLine()), IO(StdIn.readLine())).mapN(_ + _).map(println)
 
+  def sequenceTakeLast[A, B](ioa: IO[A], iob: IO[B]): IO[B] = for {
+    _ <- ioa
+    b <- iob
+  } yield b
+
+  def sequenceTakeLast_2[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa *> iob // "andThen"
+
+  def sequenceTakeLast_3[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa >> iob // "andThen" with by-name call
+
+  def sequenceTakeFirst[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa.flatMap(a => iob.map(_ => a))
+
+  def sequenceTakeFirst_v2[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa <* iob
+
+  def forever[A](io: IO[A]): IO[A] =
+    io.flatMap(_ => forever(io))
+
+  def forever_v2[A](io: IO[A]): IO[A] =
+    io >> forever_v2(io)
+
+  def forever_v3[A](io: IO[A]): IO[A] =
+    io *> forever_v3(io)
+
+  def forever_v4[A](io: IO[A]): IO[A] =
+    io.foreverM // with tail recursion
+
+  def convert[A, B](ioa: IO[A], value: B): IO[B] =
+    ioa.map(_ => value)
+
+  def convert_v2[A, B](ioa: IO[A], value: B): IO[B] =
+    ioa.as(value)
+
+  def asUnit[A](ioa: IO[A]): IO[Unit] =
+    ioa.map(_ => ())
+
+  def asUnit_v2[A](ioa: IO[A]): IO[Unit] =
+    ioa.as(()) // don't use this
+
+  def asUnit_v3[A](ioa: IO[A]): IO[Unit] =
+    ioa.void // encouraged
+
+  def sum(n: Int): Int =
+    if (n <= 0) 0
+    else n + sum(n - 1)
+
+  def sumIO(n: Int): IO[Int] = {
+    @tailrec
+    def go(remaining: Int, acc: IO[Int]): IO[Int] = {
+      if (remaining <= 0) acc
+      else go(remaining - 1, acc.map(x => x + remaining))
+    }
+
+    go(n, IO.pure(0))
+  }
+
+  def sumIO_v2(n: Int): IO[Int] = {
+    if (n <= 0) IO(0)
+    else sumIO_v2(n - 1).flatMap(x => IO(x + n))
+  }
+
+  def sumIO_v3(n: Int): IO[Int] = {
+    if (n <= 0) IO(0)
+    else for {
+      lastNumber <- IO(n)
+      prevSum <- sumIO(n - 1)
+    } yield prevSum + lastNumber
+  }
+
+  def fibonacci(n: Int): IO[BigInt] =
+    if (n <= 2) IO.pure(1)
+    else IO.defer {
+      for {
+        x <- fibonacci(n - 1)
+        y <- fibonacci(n - 2)
+      } yield x + y
+    }
+
+  def fibonacci_v2(n: Int): IO[BigInt] =
+    if (n <= 2) IO(1)
+    else for {
+      last <- IO.defer(fibonacci_v2(n - 1)) // same as .delay(...).flatten
+      prev <- IO.defer(fibonacci_v2(n - 2))
+    } yield last + prev
+
+
   def main(args: Array[String]): Unit = {
     import cats.effect.unsafe.implicits.global
-    // "end of the world"
-    println(aDelayedIO.unsafeRunSync())
+    forever(IO(println("govno"))).unsafeRunSync()
   }
+
 
 }

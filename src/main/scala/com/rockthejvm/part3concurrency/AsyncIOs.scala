da88a12bd5bd65f7b2cfcd495126f75da767679a
Курчин Иоанн Вячеславович <lvrainman@gmail.com>
2025-10-13 23:53:19 +0300

    Async IO's part2

--- src/main/scala/com/rockthejvm/part3concurrency/AsyncIOs.scala
+++ src/main/scala/com/rockthejvm/part3concurrency/AsyncIOs.scala
@@ -1,10 +1,11 @@
 package com.rockthejvm.part3concurrency
 
 import cats.effect.{IO, IOApp}
+import com.rockthejvm.utils.*
 
 import java.util.concurrent.Executors
-import scala.concurrent.ExecutionContext
-import scala.util.Try
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 object AsyncIOs extends IOApp.Simple {
 
@@ -13,22 +14,52 @@ object AsyncIOs extends IOApp.Simple {
   val ec: ExecutionContext = ExecutionContext.fromExecutorService(threadPool)
   type Callback[A] = Either[Throwable, A] => Unit
 
-  def computeMeaningOfLife(): Either[Throwable, Int] = Try {
+  def computeMeaningOfLife(): Int = {
     Thread.sleep(1000)
-    print(s"[${Thread.currentThread().getName}] computing the meaning of life on some other thread...")
+    println(s"[${Thread.currentThread().getName}] computing the meaning of life on some other thread...")
     42
+  }
+
+  def computeMeaningOfLifeEither(): Either[Throwable, Int] = Try {
+    computeMeaningOfLife()
   }.toEither
 
   def computeMolOnThreadPool(): Unit =
     threadPool.execute(() => computeMeaningOfLife())
 
   // lift computation to an IO
+  // async is a FFI
   val asyncMolIO: IO[Int] = IO.async_ { cb => // CE thread blocks (semantically) until this cb is invoked (by some other thread)
     threadPool.execute { () => // computation not managed by CE
-      val result = computeMeaningOfLife()
+      val result = computeMeaningOfLifeEither()
       cb(result) // CE thread is notified with the result
     }
   }
 
-  override def run = ???
+  def asyncToIO[A](computation: () => A)(ec: ExecutionContext): IO[A] = {
+    IO.async_[A] { cb =>
+      ec.execute { () =>
+        val result = Try(computation()).toEither
+        cb(result)
+      }
+    }
+  }
+
+  val asyncMol_v2: IO[Int] = asyncToIO(computeMeaningOfLife)(ec)
+
+  lazy val molFuture: Future[Int] = Future {
+    computeMeaningOfLife()
+  }(using ec)
+
+  def futureToIO[A](future: => Future[A], ec: ExecutionContext): IO[A] = {
+    IO.async_ { cb =>
+      future.onComplete {
+        case Success(value) => cb(Right(value))
+        case Failure(exception) => cb(Left(exception))
+      }(using ec)
+    }
+  }
+
+
+  override def run = asyncMolIO.myDebug >> IO(threadPool.shutdown())
 }

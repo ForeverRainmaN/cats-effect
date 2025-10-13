e34713054f5c45fef081a062c3c16e418aad768d
Курчин Иоанн Вячеславович <lvrainman@gmail.com>
2025-10-14 00:19:39 +0300

    Async IO's part3

--- src/main/scala/com/rockthejvm/part3concurrency/AsyncIOs.scala
+++ src/main/scala/com/rockthejvm/part3concurrency/AsyncIOs.scala
@@ -5,7 +5,7 @@ import com.rockthejvm.utils.*
 
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 object AsyncIOs extends IOApp.Simple {
 
@@ -53,13 +53,49 @@ object AsyncIOs extends IOApp.Simple {
 
   def futureToIO[A](future: => Future[A], ec: ExecutionContext): IO[A] = {
     IO.async_ { cb =>
-      future.onComplete {
-        case Success(value) => cb(Right(value))
-        case Failure(exception) => cb(Left(exception))
+      future.onComplete { tryResult =>
+        val result = tryResult.toEither
+        cb(result)
       }(using ec)
     }
   }
 
+  val asyncMolIO_v3: IO[Int] = futureToIO(molFuture, ec)
+  val asyncMolIO_v4: IO[Int] = IO.fromFuture(IO(molFuture))
 
-  override def run = asyncMolIO.myDebug >> IO(threadPool.shutdown())
+  val neverEndingIO = IO.async_(_ => ()) // no callback, no finish
+  val neverEndingIO_v2: IO[Int] = IO.never
+
+  /*
+    FULL ASYNC CALL
+   */
+  import scala.concurrent.duration.*
+
+  def demoAsyncCancelation() = {
+    val asyncMeaningOfLifeIO_v2: IO[Int] = IO.async { cb =>
+      // return IO[Option[IO[Unit]]]
+      /*
+        finalizer in case computation gets cancelled
+        finalizers are of type IO[Unit]
+        not specifying finalizer => Option[IO[Unit]]
+        creating option is an effect => IO[Option[IO[Unit]]]
+       */
+      IO {
+        threadPool.execute { () => // computation not managed by CE
+          val result = computeMeaningOfLifeEither()
+          cb(result) // CE thread is notified with the result
+        }
+      }.as(Some(IO("Cancelled").myDebug.void))
+    }
+
+
+    for {
+      fib <- asyncMeaningOfLifeIO_v2.start
+      _ <- IO.sleep(500.millis) >> IO("cancelling...").myDebug >> fib.cancel
+      _ <- fib.join
+    } yield ()
+  }
+
+
+  override def run = demoAsyncCancelation().myDebug >> IO(threadPool.shutdown())
 }

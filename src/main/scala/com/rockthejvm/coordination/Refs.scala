b4c33a23a91533b21aef8682fe48b22817e95bc5
Курчин Иоанн Вячеславович <lvrainman@gmail.com>
2025-10-20 21:52:12 +0300

    Refs part2

--- src/main/scala/com/rockthejvm/coordination/Refs.scala
+++ src/main/scala/com/rockthejvm/coordination/Refs.scala
@@ -1,6 +1,7 @@
 package com.rockthejvm.coordination
 
 import cats.effect.{IO, IOApp, Ref}
+import com.rockthejvm.utils.*
 
 object Refs extends IOApp.Simple {
   // ref = purely functional atomic reference
@@ -18,7 +19,68 @@ object Refs extends IOApp.Simple {
     ref.get // thread-safe
   }
 
-  val gsMol = atomicMol
+  val gsMol: IO[Int] = atomicMol.flatMap { ref =>
+    ref.getAndSet(43)
+  } // gets the old value, sets the new one
 
-  override def run: IO[Unit] = ???
+  // updating with a function
+  val fMol: IO[Unit] = atomicMol.flatMap { ref => ref.update(value => value * 10) }
+
+  val updatedMol: IO[Int] = atomicMol.flatMap { ref => ref.updateAndGet(value => value * 10) } // get the new value
+  // can also use getAndUpdate to get the OLD value
+
+  // modifying with a function returning a different type
+
+  // modifying with a function returning a different type
+  val modifiedMol: IO[String] = atomicMol.flatMap { ref => ref.modify(value => (value * 10, s"my current value is $value")) }
+
+  // why: concurrent + thread-safe reads / writes over shared values, in a purely functional way
+
+  def demoConcurrentWorkImpure(): IO[Unit] = {
+    var count = 0
+
+    def task(workload: String): IO[Unit] = {
+      val wordCount = workload.split(" ").length
+
+      for {
+        _ <- IO(s"Counting words for '$workload': $wordCount").myDebug
+        newCount <- IO(count + wordCount)
+        _ <- IO(s"New total: $newCount").myDebug
+        _ = IO(count += newCount)
+      } yield ()
+    }
+
+    List("I love Cats Effect", "This ref thing is useless", "Daniel writes a lot of code")
+      .map(task)
+      .parSequence
+      .void
+  }
+
+  /*
+    Drawbacks:
+  - hard to read/debug
+  - mix pure/impure code
+  - not thread safe
+   */
+
+  def demoConcurrentWorkPure(): IO[Unit] = {
+    def task(workload: String, total: Ref[IO, Int]): IO[Unit] = {
+      val wordCount = workload.split(" ").length
+
+      for {
+        _ <- IO(s"Counting words for '$workload': $wordCount").myDebug
+        newCount <- total.updateAndGet(currentCount => currentCount + wordCount)
+        _ <- IO(s"New total: $newCount").myDebug
+      } yield ()
+    }
+
+    for {
+      initialCount <- Ref[IO].of(0)
+      _ <- List("I love Cats Effect", "This ref thing is useless", "Daniel writes a lot of code")
+        .map(string => task(string, initialCount))
+        .parSequence
+    } yield ()
+  }
+
+  override def run: IO[Unit] = demoConcurrentWorkPure()
 }
